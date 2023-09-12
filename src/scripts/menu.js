944d92b50f22ce021be6dac9495d94e476badf78
mahcodes <mhmdali102@bk.ru>
2023-09-13 00:46:20 +0300

    fix

---
+++ src/scripts/menu.js
@@ -0,0 +1,4 @@
+document.getElementById("hamburger").addEventListener("click", () => {
+  document.getElementById("nav").classList.toggle("expanded");
+  document.getElementById("hamburger").classList.toggle("active");
+});
